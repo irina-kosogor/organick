eab83128c81908b97168add24abd6a3d8a3ac6e9
Irina Kosogor <i.v.kosogor@gmail.com>
2023-03-24 20:31:48 +0200

    add OfferCards section

---
+++ frontend/src/components/OfferCards/OfferCards.js
@@ -0,0 +1,42 @@
+import { ProductCard } from "../Products/components/ProductCard";
+import fakeData from "../Products/fakeData.json";
+import "./OfferCards.scss";
+
+export const OfferCards = () => {
+	const firstEFourtProducts = fakeData.slice(0, 4);
+	return (
+		<div className="offer-cards">
+			<div className="container">
+				<div className="offer-cards__subtitle">Offer</div>
+				<div className="offer-cards__title">
+					We Offer Organic For You
+				</div>
+				<div className="offer-cards__cards-wrapper">
+					{firstEFourtProducts.map((product, index) => {
+						const {
+							category,
+							"img-url": imageUrl,
+							title,
+							currency,
+							"old-price": oldPrice,
+							"new-price": newPrice,
+							rating,
+						} = product;
+						return (
+							<ProductCard
+								key={index}
+								category={category}
+								imageUrl={imageUrl}
+								title={title}
+								currency={currency}
+								oldPrice={oldPrice}
+								newPrice={newPrice}
+								rating={rating}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+};
