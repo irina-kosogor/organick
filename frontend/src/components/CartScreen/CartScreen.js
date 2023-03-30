448f74a108e75e864a9e389e549ad988259fa3cd
Irina Kosogor <i.v.kosogor@gmail.com>
2023-03-30 14:24:41 +0300

    UPD CardScreen w/list and order

--- frontend/src/components/CartScreen/CartScreen.js
+++ frontend/src/components/CartScreen/CartScreen.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import { Button } from "utils/Button/Button";
+import { ProductsList } from "./components/ProductsList/ProductsList";
+import { OrderForm } from "./components/OrderForm/OrderForm";
+
 import "./CartScreen.scss";
 
 export const CartScreen = () => {
+	const [showForm, setShowForm] = useState(false);
+
+	const handleClick = () => {
+		setShowForm((oldValue) => !oldValue);
+	};
+
 	return (
 		<div className="cart-screen">
-			<h3 className="cart-screen__title">Cart</h3>
+			<h3 className="cart-screen__title">
+				<span>Cart</span>
+			</h3>
+			<div className="cart-screen__content">
+				<ProductsList />
+				{!showForm && (
+					<Button
+						text="To order"
+						color="darkBlue"
+						onClick={handleClick}
+					/>
+				)}
+				{showForm && <OrderForm />}
+			</div>
 		</div>
 	);
 };
