d905266d26aa0ea7c8a1471a6f4b4b78a0f03466
Irina Kosogor <i.v.kosogor@gmail.com>
2023-04-04 11:49:22 +0300

    changes for Heroku Deployment

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
@@ -10,13 +11,23 @@ connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("API is running...");
-});
-
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+	);
+} else {
+	app.get("/", (req, res) => {
+		res.send("API is running...");
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(
